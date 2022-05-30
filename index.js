588605afe55340005568ccc0d6b238d233fa70b4
Devincenzi <55673616+Devincenzi@users.noreply.github.com>
2022-05-30 11:10:53 -0300

    React exercises from cod3r's course about web dev
    Initial exercises about react

---
+++ index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { BomDia, BoaNoite } from './componentes/multiplos';
+
+ReactDOM.render( < div > < BomDia nome = 'André' / > < BoaNoite nome = 'Joana' / > < /div > , document.getElementById('root'));
