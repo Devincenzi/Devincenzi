690d9377d3f1a86469a1fee34ef8df44c0a451a3
Devincenzi <55673616+Devincenzi@users.noreply.github.com>
2022-05-30 14:33:45 -0300

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import { BomDia, BoaNoite } from './componentes/multiplos';
-
-ReactDOM.render( < div > < BomDia nome = 'André' / > < BoaNoite nome = 'Joana' / > < /div > , document.getElementById('root'));
